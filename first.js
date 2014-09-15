0f3dcaaa4d6b8a3d52e3a790849d671441044797
unknown <naveen.krishnan@mwscjbtnx0126.mcwane.com>
2014-09-15 16:03:40 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,28 @@
+var http = require("http");
+var fs = require('fs');
+var port = 3000;
+var serverUrl = "127.0.0.1";
+var counter = 0;
+
+var server = http.createServer(function(req, res) {
+
+  counter++;
+  console.log("Request: " + req.url + " (" + counter + ")");
+  
+  if(req.url == "/sample.html") {
+
+    fs.readFile("sample.html", function(err, text){
+      res.setHeader("Content-Type", "text/html");
+      res.end(text);
+    });
+    return;
+
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.end("<p>Hello World. Request counter: " + counter + ".</p>");
+
+});
+
+console.log("Starting web server at " + serverUrl + ":" + port);
+server.listen(port, serverUrl);
---
+++ index.js
@@ -0,0 +1,6 @@
+
+	var server = require("./server");
+	server.start();
+	
+	
+	
